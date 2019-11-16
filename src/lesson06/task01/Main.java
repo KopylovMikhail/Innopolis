5c58cbce09be167f16d8c98472d5302aef68a23b
KopylovMikhail <kopylov_m88@mail.ru>
2019-11-17 01:14:55 +0500

    ДЗ_6.1: Написать программу, читающую текстовый файл

--- src/lesson06/task01/Main.java
+++ src/lesson06/task01/Main.java
@@ -2,28 +2,34 @@ package lesson06.task01;
 
 import java.io.*;
 import java.util.*;
+import java.util.regex.Pattern;
 
 /**
  * Программа, читающая текстовый файл и составлящая отсортированный по алфавиту список слов.
  * ДЗ_6
  * @author Михаил Копылов
- * @version 1.0
+ * @version 1.1
  */
 public class Main {
     public static void main(String[] args) throws IOException {
 
         List<String> list = new ArrayList<>();
         Set<String> set = new HashSet<>();
+        Pattern pattern = Pattern.compile("\\s"); //будем искать символ пробела с помощью конструктора регулярных выражений
 
-        BufferedReader reader = new BufferedReader(new FileReader("words.txt")); //читаем из файла
+        BufferedReader reader = new BufferedReader(new FileReader("text.txt")); //читаем из файла
         String line = "";
         while ((line = reader.readLine()) != null) { //добавляем в коллекцию Set, т.к. слова не должны повторяться
-            set.add(line.toLowerCase()); //приводим все строки к нижнему регистру
+            String[] strings = pattern.split(line,-1); //разбиваем текст на слова
+            for (String s : strings) {
+                String str = s.replaceAll("[,.!?:;&«»]", ""); //удаляем мусор из слов (знаки препинания)
+                set.add(str.toLowerCase()); //добавляем слова в коллекцию Set, приводим все строки к нижнему регистру
+            }
         }
 
         list.addAll(set); //перемещаем коллекцию Set в List для сортировки
         Collections.sort(list);
-        list.forEach(s -> System.out.println(s)); //проверяем результат сортировки
+//        list.forEach(s -> System.out.println(s)); //проверяем результат сортировки
 
         BufferedWriter writer = new BufferedWriter(new FileWriter("words_new.txt")); //записываем в файл
         for (String s : list) {
